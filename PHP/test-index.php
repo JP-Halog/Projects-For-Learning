9f63768de6e4b0b0bad03810286b23c8b66ef039
JP-Halog <jdh0102@auburn.edu>
2024-01-25 13:45:24 -0600

    Create test-index.php
    First test file for a php project.

---
+++ PHP/test-index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cool Website</title>
+</head>
+<body>
+    <h1>Welcome to the Cool Website!</h1>
+    
+    <button onclick="displayMessage()">Click me!</button>
+    
+    <script>
+        function displayMessage() {
+            alert("This works!");
+        }
+    </script>
+</body>
+</html>
